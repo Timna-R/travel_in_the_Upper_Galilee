aaf748548615ccdb770b6137bbbe7cc810ad63f7
Timna <timnar312@gmail.com>
2023-07-10 14:21:17 +0300

    Feature: Languages

---
+++ index.js
@@ -0,0 +1,20 @@
+const en_btn = document.getElementById('en_btn');
+const he_btn = document.getElementById('he_btn');
+
+const english = document.querySelectorAll('.english');
+const hebrew = document.querySelectorAll('.hebrew');
+
+en_btn.addEventListener('click', function onClick() {
+
+    english.forEach(elem => elem.style.display = 'block');
+    hebrew.forEach(elem => elem.style.display = 'none');
+
+    // he_btn.classList.remove("btn_active");
+    // he_btn.classList.add("btn_active");
+});
+he_btn.addEventListener('click', function onClick() {
+    english.forEach(elem => elem.style.display = 'none');
+    hebrew.forEach(elem => elem.style.display = 'block');
+    // he_btn.classList.add("btn_active");
+    // he_btn.classList.remove("btn_active");
+});
