6701940a3ccaf07e95e850e42be8f78754e7ad16
Timna <timnar312@gmail.com>
2023-07-12 11:25:05 +0300

    places

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const en_btn = document.getElementById('en_btn');
 const he_btn = document.getElementById('he_btn');
-
 const english = document.querySelectorAll('.english');
 const hebrew = document.querySelectorAll('.hebrew');
 
@@ -9,12 +8,12 @@ en_btn.addEventListener('click', function onClick() {
     english.forEach(elem => elem.style.display = 'block');
     hebrew.forEach(elem => elem.style.display = 'none');
 
-    // he_btn.classList.remove("btn_active");
+    // place.classList.remove("place_hebrew");
     // he_btn.classList.add("btn_active");
 });
 he_btn.addEventListener('click', function onClick() {
     english.forEach(elem => elem.style.display = 'none');
     hebrew.forEach(elem => elem.style.display = 'block');
-    // he_btn.classList.add("btn_active");
+    // place.classList.add("place_hebrew");
     // he_btn.classList.remove("btn_active");
 });
