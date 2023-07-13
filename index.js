d80fb87d29551491ee5d001fb0aa5f921c35ca68
Timna <timnar312@gmail.com>
2023-07-13 14:03:50 +0300

    lang - btn style

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ en_btn.addEventListener('click', function onClick() {
     english.forEach(elem => elem.style.display = 'block');
     hebrew.forEach(elem => elem.style.display = 'none');
 
-    // place.classList.remove("place_hebrew");
-    // he_btn.classList.add("btn_active");
+    he_btn.classList.remove("btn_active");
+    en_btn.classList.add("btn_active");
 });
 he_btn.addEventListener('click', function onClick() {
     english.forEach(elem => elem.style.display = 'none');
     hebrew.forEach(elem => elem.style.display = 'block');
-    // place.classList.add("place_hebrew");
-    // he_btn.classList.remove("btn_active");
+    he_btn.classList.add("btn_active");
+    en_btn.classList.remove("btn_active");
 });
